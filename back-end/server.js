a84e0cd4d021fcbf00c1d2a87a36a2528e75a4e2
Carlos Eduardo Papacidero <carlos.papacidero@venturus.org.br>
2016-09-09 16:36:51 -0300

    Save to Mongo from the endpoint

--- back-end/server.js
+++ back-end/server.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var mongo = require('mongodb').MongoClient;
+
+var database;
 
 app.use(bodyParser.json());
 
@@ -12,7 +15,18 @@ app.use(function(req, res, next){
 
 app.post('/api/message', function(req,res){
     console.log(req.body);
+    database.collection('messages').insertOne(req.body);
     res.status(200);
+});
+
+mongo.connect("mongodb://localhost:27017/test", function(err,db){
+    if (!err) {
+        console.log("We are connected to mongo");
+        database = db;
+        // db.collection('messages').insertOne({'msg':'test'});
+    } else {
+        console.error("We are not connected to mongo");
+    }
 });
 
 var server = app.listen(5000, function(){
